f972d9a4716d2351ab603ce348e0c6105f1c28b4
Paweł Partyka <partyka95@icloud.com>
2017-02-11 15:31:18 +0100

    Init

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+const babel = require('rollup-plugin-babel');
+const replace = require('rollup-plugin-replace');
+const version = process.env.VERSION || require('./package.json').version;
+
+module.exports = {
+    entry: 'src/index.js',
+    dest: 'dist/vue-decorators.js',
+    format: 'umd',
+    moduleName: 'vue-decorators',
+    external: ['vue', 'vuex'],
+    plugins: [
+        replace({__VERSION__: version}),
+        babel()
+    ],
+    banner: `/**
+ * Vue-decorators v${version}
+ * (c) ${new Date().getFullYear()} Paweł Partyka
+ * @license MIT
+ */`
+};
